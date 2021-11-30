d3bd627d3b471513da209bb680c6bb452d7d06cb
CJ McCaskill <cjmccaskill@gmail.com>
2021-11-30 11:33:22 -0600

    Add handle change function

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -45,6 +45,11 @@ class SignUp extends React.Component {
     }
   };
 
+  handleChange = (event) => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  };
+
   render() {
     const { displayName, email, password, confirmPassword } = this.state;
     return (
@@ -84,7 +89,7 @@ class SignUp extends React.Component {
             label="Confirm Password"
             required
           />
-          <CustomButton type="submit"> SIGN UP </CustomButton>
+          <CustomButton type="submit">SIGN UP</CustomButton>
         </form>
       </div>
     );
